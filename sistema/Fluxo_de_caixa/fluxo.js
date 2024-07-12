0da394730ac3f64d5f6809060c69e914f091a917
SandyNS1989 <166540033+SandyNS1989@users.noreply.github.com>
2024-07-12 23:30:58 +0000

    iframe movel para notebooks

--- sistema/Fluxo_de_caixa/fluxo.js
+++ sistema/Fluxo_de_caixa/fluxo.js
@@ -188,6 +188,8 @@ document.getElementById("ch-side").addEventListener("change", event => {
   }
 })
 
+
+
 document.getElementById("btn_voltar_flx").addEventListener("click", () => {
   window.location.href = '../Menu/menu.html';
 }); 
@@ -195,3 +197,46 @@ document.getElementById("btn_voltar_flx").addEventListener("click", () => {
 document.getElementById("open-chat-btn1").addEventListener("click", () => {
   window.location.href = '../chat/chat.html'
 })
+
+const draggable = document.getElementById('draggable-container');
+let isDraggable = true;
+let mouseDown = false;
+
+draggable.onmousedown = function(event) {
+    if (!isDraggable) return;
+
+    mouseDown = true;
+    event.preventDefault();
+    
+    let shiftX = event.clientX - draggable.getBoundingClientRect().left;
+    let shiftY = event.clientY - draggable.getBoundingClientRect().top;
+
+    function moveAt(pageX, pageY) {
+        draggable.style.left = pageX - shiftX + 'px';
+        draggable.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        if (mouseDown) {
+            moveAt(event.pageX, event.pageY);
+        }
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    draggable.onmouseup = function() {
+        mouseDown = false;
+        document.removeEventListener('mousemove', onMouseMove);
+    };
+};
+
+draggable.ondragstart = function() {
+    return false;
+};
+
+document.addEventListener('click', function(event) {
+    isDraggable = !isDraggable;
+    draggable.style.cursor = isDraggable ? 'move' : 'default';
+});
+
+
